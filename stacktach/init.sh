44c4c1404a3a13cc49ff067b605c09d62c2c59fc
Andrew Melton <ramielrowe@gmail.com>
2013-05-09 21:19:28 +0000

    Correcting stacktach init

--- stacktach/init.sh
+++ stacktach/init.sh
@@ -1,9 +1,11 @@
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password $MYSQL_PASSWORD'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $MYSQL_PASSWORD'
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password '$MYSQL_PASSWORD'"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password '$MYSQL_PASSWORD'"
 apt-get -q -y install mysql-server
 mysql -u root --password=$MYSQL_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $STACKTACH_DB_NAME;"
 
 git clone $STACKTACH_REPO ~/stacktach_app
+
+easy_install -U distribute
 
 pip install -r ~/stacktach_app/etc/pip-requires.txt
 
