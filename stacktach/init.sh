34543395d19b30d880ea0449b9f52bc5a5ee64a1
Andrew Melton <ramielrowe@gmail.com>
2013-05-09 20:27:25 +0000

    Adding stacktach verifier

--- stacktach/init.sh
+++ stacktach/init.sh
@@ -10,4 +10,7 @@ pip install -r ~/stacktach_app/etc/pip-requires.txt
 python ~/stacktach_app/manage.py syncdb
 
 envsubst < stacktach/etc/sample_stacktach_worker_config.json > ~/stacktach_app/etc/stacktach_worker_config.json
-export STACKTACH_WORKER_CONFIG=~/stacktach_app/etc/stacktach_worker_config.json
+STACKTACH_WORKER_CONFIG=~/stacktach_app/etc/stacktach_worker_config.json
+
+envsubst < stacktach/etc/sample_stacktach_verifier_config.json > ~/stacktach_app/etc/stacktach_verifier_config.json
+STACKTACH_VERIFIER_CONFIG=~/stacktach_app/etc/stacktach_verifier_config.json
--- stacktach/run.sh
+++ stacktach/run.sh
@@ -6,4 +6,7 @@ sleep 1.5
 screen -S stacktach -p django -X stuff "python ~/stacktach_app/manage.py runserver 198.101.244.161:80$NL"
 screen -S stacktach -X screen -t worker
 sleep 1.5
-screen -S stacktach -p worker -X stuff "export STACKTACH_DEPLOYMENTS_FILE=~/stacktach_app/etc/stacktach_worker_config.json && python ~/stacktach_app/worker/start_workers.py$NL"
+screen -S stacktach -p worker -X stuff "export STACKTACH_DEPLOYMENTS_FILE=$STACKTACH_WORKER_CONFIG && python ~/stacktach_app/worker/start_workers.py$NL"
+screen -S stacktach -X screen -t verifier
+sleep 1.5
+screen -S stacktach -p verifier -X stuff "export STACKTACH_VERIFIER_CONFIG=$STACKTACH_VERIFIER_CONFIG && python ~/stacktach_app/verifier/start_verifier.py$NL"
